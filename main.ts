c13e11fb49d03b28932d02fa3dacbc50c916517f
cavaldos <khanhnguyen297kn@gmail.com>
2023-09-26 16:19:25 +0700

    init

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 const morgan = require("morgan");
-app.use(morgan("tiny"));
+const cors = require("cors");
 
-const port = process.env.PORT || 3000;
+dotenv.config();
+app.use(morgan("tiny"));
+app.use(cors());
+
+
+app.use(express.urlencoded({ extended: true }));
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+  console.log(` ✅  Listening on port  ${port} 🚀`);
 });
